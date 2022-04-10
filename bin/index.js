9ba8373564aa31adc72bfc30c0988bbd16352c29
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2022-04-10 07:39:14 +0100

    Fix codacy issues

--- bin/index.js
+++ bin/index.js
@@ -1,16 +1,16 @@
 /*jshint esversion: 8 */
 import {
   Command as c
-} from 'commander';
+} from "commander";
 import {
   memorablePassword
-} from '../lib/memorable-password.js';
+} from "../lib/memorable-password.js";
 import {
   complexPassword
-} from '../lib/complex-password.js';
+} from "../lib/complex-password.js";
 import {
   base64Password
-} from '../lib/base64-password.js';
+} from "../lib/base64-password.js";
 
 // Global Variables
 global.log = (arg) => console.log(arg);
@@ -18,29 +18,20 @@ global.args = process.argv.slice(2);
 const program = new c();
 
 (async () => {
+  program
+    .version("1.0.0")
+    .option("-t, --type <type>", "Specify a type either base64, complex or memorable")
+    .option("-i, --iteration <numbers>", "Specify a number of iteration")
+    .option("-s, --separator <char>", "Specify a character for the separator")
+    .parse(process.argv);
 
-  try {
-    program
-      .version("1.0.0")
-      .option('-t, --type <type>', 'Add type either memorable or complex')
-      .option('-i, --iteration', 'Add iteration')
-      .option('-s, --separator <char>', 'Add separator')
-      .parse(process.argv);
-
-    if (args.length === 0) {
-      program.help();
-    } else if (args[1] == "memorable") {
-      memorablePassword();
-    } else if (args[1] === "complex") {
-      complexPassword();
-    } else if (args[1] === "base64") {
-      base64Password();
-    }
-
-
-  } catch (error) {
-    log(error.stack);
-    log(error);
+  if (args.length === 0) {
+    program.help();
+  } else if (args[1] == "memorable") {
+    memorablePassword();
+  } else if (args[1] === "complex") {
+    complexPassword();
+  } else if (args[1] === "base64") {
+    base64Password();
   }
-
 })();
