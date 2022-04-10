5cc17de767a95a3809b149683d608ba94dabccb6
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2022-04-10 14:06:09 +0100

    Fix test

--- test/test-title-case.js
+++ test/test-title-case.js
@@ -3,8 +3,7 @@ import { titleCase } from "../lib/title-case.js";
 import * as chai from "chai";
 let expect = chai.expect;
 
-// Begin a test for titleCase()
-
+// titleCase() test
 describe("titleCase(string)", function () {
   it("receives one argument and returns it in title case", function () {
     expect(titleCase("HELLO")).equal("Hello");
