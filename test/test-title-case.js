c9a0e4b536a201a3a7c3f18a47792ef81990b7ef
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2022-04-10 14:23:52 +0100

    Add minor tests

--- test/test-title-case.js
+++ test/test-title-case.js
@@ -2,10 +2,31 @@
 import { titleCase } from "../lib/title-case.js";
 import * as chai from "chai";
 let expect = chai.expect;
+describe('Mocha test', function(){
+  it('should load mocha', function(){
+    expect(true);
+  });
+});
 
 // titleCase() test
 describe("titleCase(string)", function () {
-  it("receives one argument and returns it in title case", function () {
-    expect(titleCase("HELLO")).equal("Hello");
+   it('should return a string', function(){
+    let str = "HELLO";
+    expect(titleCase(str)).to.be.a('string');
+   });
+
+  it("should return a titleized string for one lowercase word", function () {
+    let str = "hello";
+    expect(titleCase(str)).equal("Hello");
+  });
+
+  it("should return a titleized string for one uppercase word", function () {
+    let str = "HELLO";
+    expect(titleCase(str)).equal("Hello");
+  });
+
+  it("should return a titleized string for a sentence of words", function () {
+    let str = "hello world";
+    expect(titleCase(str)).equal("Hello world");
   });
 });
