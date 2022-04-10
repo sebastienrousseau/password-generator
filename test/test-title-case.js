005029a5fe050f4892cb7d5a87c4d83830104195
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2022-04-10 14:02:56 +0100

    Fix codacity issues

--- test/test-title-case.js
+++ test/test-title-case.js
@@ -1,12 +1,12 @@
 /*jshint esversion: 8 */
 import { titleCase } from "../lib/title-case.js";
-import * as chai from 'chai';
+import * as chai from "chai";
 let expect = chai.expect;
 
 // Begin a test for titleCase()
 
-describe('titleCase(string)', function () {
-  it('receives one argument and returns it in title case', function () {
-    expect(titleCase('HELLO')).equal('Hello');
+describe("titleCase(string)", function () {
+  it("receives one argument and returns it in title case", function () {
+    expect(titleCase("HELLO")).equal("Hello");
   });
 });
