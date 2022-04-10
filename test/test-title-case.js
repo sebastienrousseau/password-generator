26f3a2a942e5d8c44fe63d76d73130b5e40c609a
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2022-04-10 14:34:16 +0100

    Fix styling

--- test/test-title-case.js
+++ test/test-title-case.js
@@ -4,29 +4,26 @@ import * as chai from "chai";
 let expect = chai.expect;
 
 // mocha() test
-describe("mocha()", function(){
-  it("should load mocha", function(){
+describe("mocha () ", function () {
+  it("should load mocha", function () {
     expect(true);
   });
 });
 
 // titleCase() test
-describe("titleCase(string)", function () {
-   it("should return a string", function(){
+describe("titleCase (string) ", function () {
+   it("should return a string", function () {
     let str = "HELLO";
     expect(titleCase(str)).to.be.a("string");
    });
-
   it("should return a titleized string for one lowercase word", function () {
     let str = "hello";
     expect(titleCase(str)).equal("Hello");
   });
-
   it("should return a titleized string for one uppercase word", function () {
     let str = "HELLO";
     expect(titleCase(str)).equal("Hello");
   });
-
   it("should return a titleized string for a sentence of words", function () {
     let str = "hello world";
     expect(titleCase(str)).equal("Hello world");
