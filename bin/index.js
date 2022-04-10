964faa5fceb7bec416f4aa0d5d473eebf3cb441a
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2022-04-10 08:19:24 +0100

    Fix codacy issues

--- bin/index.js
+++ bin/index.js
@@ -1,8 +1,8 @@
 /*jshint esversion: 8 */
-import {base64Password} from "../lib/base64-password.js";
-import {Command as c} from "commander";
-import {complexPassword} from "../lib/complex-password.js";
-import {memorablePassword} from "../lib/memorable-password.js";
+import { base64Password } from "../lib/base64-password.js";
+import { Command as c } from "commander";
+import { complexPassword } from "../lib/complex-password.js";
+import { memorablePassword } from "../lib/memorable-password.js";
 
 // Initializing Variables
 const args = process.argv.slice(2);
@@ -11,7 +11,7 @@ const program = new c();
 (async () => {
   program
     .version("1.0.0")
-    .option("-t, --type <type>", "Specify a type either base64, complex or memorable")
+    .option("-t, --type <type>", "Specify a type (base64, complex, memorable)")
     .option("-i, --iteration <numbers>", "Specify a number of iteration")
     .option("-s, --separator <char>", "Specify a character for the separator")
     .parse(process.argv);
