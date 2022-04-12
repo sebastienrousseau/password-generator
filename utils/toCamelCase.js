0c53a93a9bee39825a5f4eece6ceb86bf63f6e85
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2022-04-12 14:00:41 +0100

    Add debugging

--- utils/toCamelCase.js
+++ utils/toCamelCase.js
@@ -12,17 +12,17 @@ export const toCamelCase = (str) => {
         /[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+|[A-Z]|\d+/g
       )
       .map((x) => x.slice(0, 1).toUpperCase() + x.slice(1).toLowerCase())
-      .join('');
+      .join("");
   return s.slice(0, 1).toLowerCase() + s.slice(1);
 };
 
-console.log(toCamelCase("passwordGenerator")); //
-console.log(toCamelCase("password.generator"));
-console.log(toCamelCase("--PASSWORD-GENERATOR--"));
-console.log(toCamelCase("password-generator"));
-console.log(toCamelCase("PasswordGenerator"));
-console.log(toCamelCase("Password generator"));
-console.log(toCamelCase("password_generator"));
-console.log(toCamelCase("password generator"));
-console.log(toCamelCase("Password Generator"));
-console.log(toCamelCase("PASSWORD GENERATOR"));
+// console.log(toCamelCase("passwordGenerator")); //
+// console.log(toCamelCase("password.generator"));
+// console.log(toCamelCase("--PASSWORD-GENERATOR--"));
+// console.log(toCamelCase("password-generator"));
+// console.log(toCamelCase("PasswordGenerator"));
+// console.log(toCamelCase("Password generator"));
+// console.log(toCamelCase("password_generator"));
+// console.log(toCamelCase("password generator"));
+// console.log(toCamelCase("Password Generator"));
+// console.log(toCamelCase("PASSWORD GENERATOR"));
