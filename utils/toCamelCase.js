9e33f5b051175926c3dedad62c232b7b3a61c0f5
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2022-04-12 14:00:03 +0100

    Replace `.map(x` with `··.map((x)`

--- utils/toCamelCase.js
+++ utils/toCamelCase.js
@@ -11,7 +11,7 @@ export const toCamelCase = (str) => {
       .match(
         /[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+|[A-Z]|\d+/g
       )
-      .map(x => x.slice(0, 1).toUpperCase() + x.slice(1).toLowerCase())
+      .map((x) => x.slice(0, 1).toUpperCase() + x.slice(1).toLowerCase())
       .join('');
   return s.slice(0, 1).toLowerCase() + s.slice(1);
 };
