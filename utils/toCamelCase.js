583b67441735591aa05bb0db69e1a861be3b4eff
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2022-04-12 14:34:53 +0100

    Remove the expression can be replaced with '[A-Z]' (no quantifier) without affecting the lookaround.

--- utils/toCamelCase.js
+++ utils/toCamelCase.js
@@ -5,7 +5,7 @@
  * @param {String} str The text to be converted to camel case.
  */
 export const toCamelCase = (str) => {
-  const r = /[A-Z]{2,}(?=[A-Z]+|\b)|[A-Z]?[a-z]+|[A-Z]|\d+/g;
+  const r = /[A-Z]{2,}(?=[A-Z]+|\b)|[A-Z]?[a-z]+||\d+/g;
   const s =
     str &&
     str
@@ -15,13 +15,13 @@ export const toCamelCase = (str) => {
   return s.slice(0, 1).toLowerCase() + s.slice(1);
 };
 
-// console.log(toCamelCase("passwordGenerator"));
-// console.log(toCamelCase("password.generator"));
-// console.log(toCamelCase("--PASSWORD-GENERATOR--"));
-// console.log(toCamelCase("password-generator"));
-// console.log(toCamelCase("PasswordGenerator"));
-// console.log(toCamelCase("Password generator"));
-// console.log(toCamelCase("password_generator"));
-// console.log(toCamelCase("password generator"));
-// console.log(toCamelCase("Password Generator"));
-// console.log(toCamelCase("PASSWORD GENERATOR"));
+// console.log(toCamelCase("passwordGenerator"));      // ✔ should convert a camel case string to camelcase
+// console.log(toCamelCase("password.generator"));     // ✔ should convert a dot case string to camelcase
+// console.log(toCamelCase("--PASSWORD-GENERATOR--")); // ✔ should convert a junk case string to camelcase
+// console.log(toCamelCase("password-generator"));     // ✔ should convert a kebab case string to camelcase
+// console.log(toCamelCase("PasswordGenerator"));      // ✔ should convert a pascal case string to camelcase
+// console.log(toCamelCase("Password generator"));     // ✔ should convert a sentence case string to camelcase
+// console.log(toCamelCase("password_generator"));     // ✔ should convert a snake case string to camelcase
+// console.log(toCamelCase("password generator"));     // ✔ should convert a space case string to camelcase
+// console.log(toCamelCase("Password Generator"));     // ✔ should convert a title case string to camelcase
+// console.log(toCamelCase("PASSWORD GENERATOR"));     // ✔ should convert a uppercase case string to camelcase
