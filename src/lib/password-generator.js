6c43023f586ae9367bb5a7ffa3624f8cc929ce41
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2022-04-19 21:40:35 +0100

    Fix styling issues

--- src/lib/password-generator.js
+++ src/lib/password-generator.js
@@ -15,10 +15,11 @@ const actions = {
 const args = process.argv.slice(2);
 const bool = Object.prototype.hasOwnProperty.call(actions, args[1]);
 const pkg = JSON.parse(
-  readFileSync(join(process.cwd(), "/package.json"), "utf8"));
+  readFileSync(join(process.cwd(), "/package.json"), "utf8")
+);
 const program = new c();
-export function passwordGenerator(){
-
+export function passwordGenerator()
+{
   program
     .version(pkg.version)
     .option("-t, --type <type>", "specify a password type")
