5f839f69813d61646e7559248a6aba341a9813d9
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2022-04-25 20:39:02 +0100

    Reformatting

--- index.js
+++ index.js
@@ -14,17 +14,21 @@ program
   .option("-l, --length <numbers>", "specify a length for each iteration")
   .option("-i, --iteration <numbers>", "specify a number of iteration")
   .option("-s, --separator <char>", "specify a character for the separator")
-  .action(async(args) => {
+  .action(async (args) => {
     if (args.type === "base64") {
-      const { base64Password } = import ("./src/lib/base64-password.js");
+      const { base64Password } = import("./src/lib/base64-password.js");
       base64Password;
     } else if (args.type === "strong") {
-      const { strongPassword } = import ("./src/lib/strong-password.js");
+      const { strongPassword } = import("./src/lib/strong-password.js");
       strongPassword;
     } else if (args.type === "memorable") {
-      const { memorablePassword } = import ("./src/lib/memorable-password.js");
+      const { memorablePassword } = import("./src/lib/memorable-password.js");
       memorablePassword;
-    } else if (args.type !== "base64" || args.type !== "memorable" || args.type !== "strong") {
+    } else if (
+      args.type !== "base64" ||
+      args.type !== "memorable" ||
+      args.type !== "strong"
+    ) {
       program.help();
     }
   })
