1baab84348757f347a0efa869de438367fa8fb2c
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2022-04-30 19:21:32 +0100

    Fixing codacy

--- src/bin/password-generator.js
+++ src/bin/password-generator.js
@@ -1,16 +1,21 @@
 // Initializing Variables
 const args = process.argv.slice(2);
+
 const PasswordGenerator = async(data) => {
   if (data.type) {
     let genPassword = "../lib/"+ data.type + "-password.js";
     const run = async() => {
-      genPassword = await import (genPassword); genPassword;
+      genPassword = await import (genPassword);
+      genPassword;
     };
     run();
   }
 };
-PasswordGenerator(
-  { type: args[1], length: args[3], iteration: args[5], separator: args[7]}
-);
+PasswordGenerator({
+  type: args[1],
+  length: args[3],
+  iteration: args[5],
+  separator: args[7]
+});
 export default PasswordGenerator;
 
