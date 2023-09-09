732243ef2e46e8d1d4c9cbd64ba58b346c750ab6
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2023-09-09 12:36:48 +0100

    feat(password-generator): improved code quality and security of base64 password generator

--- src/lib/base64-password.js
+++ src/lib/base64-password.js
@@ -1,26 +1,73 @@
+// Copyright © 2022-2023 Password Generator. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: MIT
+
 import crypto from "crypto";
+import clipboardy from "clipboardy";
 
-// Initializing Variables
-const args = process.argv.slice(2);
-
-const base64Password = async(data) => {
-  // Generating a base64 variable.
-  let base64 = await crypto.randomBytes(256).toString("base64");
-  // Initializing a base64 password
-  base64 = base64
-    .match(new RegExp(".{1," + data.length + "}", "g"))
-    .slice(0, data.iteration)
-    .join(data.separator);
-  console.log(base64);
-  return base64;
+/**
+ * Generate a random base64 string.
+ *
+ * @return {string} The generated base64 string.
+ */
+const generateRandomBase64String = () => {
+  return crypto.randomBytes(256).toString("base64");
 };
-export default base64Password;
 
-if (args) {
-  let data = args;
-  data.length = data[3];
-  data.iteration = data[5];
-  data.separator = data[7];
-  base64Password(data);
+/**
+ * Split a base64 string into substrings of the specified length.
+ *
+ * @param {string} base64String The base64 string to split.
+ * @param {number} length The length of each substring.
+ * @return {Array<string>} The substrings.
+ */
+const splitBase64String = (base64String, length) => {
+  const substrings = base64String.match(new RegExp(`.{1,${length}}`, "g"));
+  return substrings || [];
+};
+
+/**
+ * Generate a password using random Base64 characters.
+ *
+ * @param {Object} options - Configuration options for password generation.
+ * @param {number} options.length - The length of each password chunk.
+ * @param {number} options.iteration - The number of password chunks.
+ * @param {string} options.separator - The separator between password chunks.
+ * @return {string} The generated password.
+ */
+const base64Password = ({ length, iteration, separator }) => {
+  const base64String = generateRandomBase64String();
+  const substrings = splitBase64String(base64String, length);
+  const password = substrings.slice(0, iteration).join(separator);
+  return password;
+};
+
+// Parse command-line arguments
+const args = process.argv.slice(2);
+const data = {};
+
+for (let i = 0; i < args.length; i += 2) {
+  const key = args[i].replace("-", "");
+  const value = args[i + 1];
+  data[key] = value;
 }
 
+if (data.t !== "base64" || !data.l || !data.i || !data.s) {
+  console.error(
+    "Usage: node . -t base64 -l <length> -i <iteration> -s <separator>"
+  );
+  process.exit(1);
+}
+
+// Generate the password
+const generatedPassword = base64Password({
+  length: parseInt(data.l, 10),
+  iteration: parseInt(data.i, 10),
+  separator: data.s
+});
+
+// Copy the password to clipboard
+clipboardy.writeSync(generatedPassword);
+
+// Print the generated password
+console.log(generatedPassword);
