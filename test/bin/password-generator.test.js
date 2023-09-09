bfd39ae6e9b466fd3ed175ebb2bcc4c3960b0df2
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2023-09-09 22:31:18 +0100

    fix(password-generator): test updates

--- test/bin/password-generator.test.js
+++ test/bin/password-generator.test.js
@@ -5,47 +5,4 @@ describe('PasswordGenerator', function () {
   it('should be a function', function () {
     expect(PasswordGenerator).to.be.a('function');
   });
-
-  it('should generate a password when given valid arguments', async function () {
-    // Replace with valid arguments for your use case
-    const data = {
-      type: 'your-type',
-      length: 'your-length',
-      iteration: 'your-iteration',
-      separator: 'your-separator',
-    };
-
-    // Use try-catch to handle any potential exceptions from dynamic imports
-    try {
-      const result = await PasswordGenerator(data);
-      // Assert that the result is what you expect
-      expect(result).to.equal('expected-password');
-    } catch (error) {
-      // Handle any exceptions here, e.g., if the dynamic import fails
-      // You might want to add specific assertions for error handling
-      expect.fail('An error occurred: ' + error.message);
-    }
-  });
-
-  it('should handle missing or invalid arguments gracefully', async function () {
-    // Test cases for different argument scenarios
-    const testCases = [
-      // Test case 1: Missing "type" argument
-      { data: { length: 'your-length' }, expectedResult: 'expected-error-message' },
-
-      // Test case 2: Invalid "type" argument
-      { data: { type: 'invalid-type', length: 'your-length' }, expectedResult: 'expected-error-message' },
-
-      // Add more test cases for other missing or invalid arguments
-    ];
-
-    for (const testCase of testCases) {
-      try {
-        const result = await PasswordGenerator(testCase.data);
-        expect(result).to.equal(testCase.expectedResult);
-      } catch (error) {
-        expect(error.message).to.equal(testCase.expectedResult);
-      }
-    }
-  });
 });
