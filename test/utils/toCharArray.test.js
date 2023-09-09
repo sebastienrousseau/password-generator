3fd442f9b791a49b5f181d44f32d3b9a2c8b3adc
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2023-09-09 18:34:01 +0100

    test(password generator): fixed toCharArray

--- test/utils/toCharArray.test.js
+++ test/utils/toCharArray.test.js
@@ -1,7 +1,21 @@
 import { toCharArray } from "../../src/utils/toCharArray/toCharArray.js";
+import { assert } from 'chai';
+
+const strings = {
+  camel: "passwordGenerator",
+  dot: "password.generator",
+  junk: "--PASSWORD-GENERATOR--",
+  kebab: "password-generator",
+  pascal: "PasswordGenerator",
+  sentence: "Password generator",
+  snake: "password_generator",
+  space: "password generator",
+  title: "Password Generator",
+  uppercase: "PASSWORD GENERATOR",
+};
 
 // Test type of toCharArray() function
-describe("Running test type of toCharArray() function \n", function () {
+describe("Running test type of toCharArray() function", function () {
   for (let key in strings) testArray(key);
 });
 
@@ -10,15 +24,14 @@ describe("Running test type of toCharArray() function \n", function () {
  *
  * @param {String} key
  */
-
 function testArray(key) {
-  it("should validate that the conversion of a " + key + " case string should be of type array", function () {
-    assert.typeOf(toCharArray(strings[key]), 'array' );
+  it(`should validate that the conversion of a ${key} case string should be of type array`, function () {
+    assert.typeOf(toCharArray(strings[key]), 'array');
   });
 }
 
 // toCharArray() test function
-describe("Running toCharArray () function \n", function () {
+describe("Running toCharArray() function", function () {
   for (let key in strings) testFunction(key);
 });
 
@@ -27,18 +40,21 @@ describe("Running toCharArray () function \n", function () {
  *
  * @param {String} key
  */
-
 function testFunction(key) {
-  it("should validate that the conversion of a " + key + " case string should be an array of characters.", function () {
-    assert.deepEqual(toCharArray(strings.camel), ['p', 'a', 's', 's', 'w', 'o', 'r', 'd', 'G', 'e', 'n', 'e', 'r', 'a', 't', 'o', 'r'] );
-    assert.deepEqual(toCharArray(strings.dot), ['p', 'a', 's', 's', 'w', 'o', 'r', 'd', '.', 'g', 'e', 'n', 'e', 'r', 'a', 't', 'o', 'r'] );
-    assert.deepEqual(toCharArray(strings.junk), ['-', '-', 'P', 'A', 'S', 'S', 'W', 'O', 'R', 'D', '-', 'G', 'E', 'N', 'E', 'R', 'A', 'T', 'O', 'R', '-', '-'] );
-    assert.deepEqual(toCharArray(strings.kebab), ['p', 'a', 's', 's', 'w', 'o', 'r', 'd', '-', 'g', 'e', 'n', 'e', 'r', 'a', 't', 'o', 'r'] );
-    assert.deepEqual(toCharArray(strings.pascal), ['P', 'a', 's', 's', 'w', 'o', 'r', 'd', 'G', 'e', 'n', 'e', 'r', 'a', 't', 'o', 'r'] );
-    assert.deepEqual(toCharArray(strings.sentence), ['P', 'a', 's', 's', 'w', 'o', 'r', 'd', ' ', 'g', 'e', 'n', 'e', 'r', 'a', 't', 'o', 'r'] );
-    assert.deepEqual(toCharArray(strings.snake), ['p', 'a', 's', 's', 'w', 'o', 'r', 'd', '_', 'g', 'e', 'n', 'e', 'r', 'a', 't', 'o', 'r'] );
-    assert.deepEqual(toCharArray(strings.space), ['p', 'a', 's', 's', 'w', 'o', 'r', 'd', ' ', 'g', 'e', 'n', 'e', 'r', 'a', 't', 'o', 'r'] );
-    assert.deepEqual(toCharArray(strings.title), ['P', 'a', 's', 's', 'w', 'o', 'r', 'd', ' ', 'G', 'e', 'n', 'e', 'r', 'a', 't', 'o', 'r'] );
-    assert.deepEqual(toCharArray(strings.uppercase), ['P', 'A', 'S', 'S', 'W', 'O', 'R', 'D', ' ', 'G', 'E', 'N', 'E', 'R', 'A', 'T', 'O', 'R'] );
+  it(`should validate that the conversion of a ${key} case string should be an array of characters`, function () {
+    const expectedResults = {
+      camel: ['p', 'a', 's', 's', 'w', 'o', 'r', 'd', 'G', 'e', 'n', 'e', 'r', 'a', 't', 'o', 'r'],
+      dot: ['p', 'a', 's', 's', 'w', 'o', 'r', 'd', '.', 'g', 'e', 'n', 'e', 'r', 'a', 't', 'o', 'r'],
+      junk: ['-', '-', 'P', 'A', 'S', 'S', 'W', 'O', 'R', 'D', '-', 'G', 'E', 'N', 'E', 'R', 'A', 'T', 'O', 'R', '-', '-'],
+      kebab: ['p', 'a', 's', 's', 'w', 'o', 'r', 'd', '-', 'g', 'e', 'n', 'e', 'r', 'a', 't', 'o', 'r'],
+      pascal: ['P', 'a', 's', 's', 'w', 'o', 'r', 'd', 'G', 'e', 'n', 'e', 'r', 'a', 't', 'o', 'r'],
+      sentence: ['P', 'a', 's', 's', 'w', 'o', 'r', 'd', ' ', 'g', 'e', 'n', 'e', 'r', 'a', 't', 'o', 'r'],
+      snake: ['p', 'a', 's', 's', 'w', 'o', 'r', 'd', '_', 'g', 'e', 'n', 'e', 'r', 'a', 't', 'o', 'r'],
+      space: ['p', 'a', 's', 's', 'w', 'o', 'r', 'd', ' ', 'g', 'e', 'n', 'e', 'r', 'a', 't', 'o', 'r'],
+      title: ['P', 'a', 's', 's', 'w', 'o', 'r', 'd', ' ', 'G', 'e', 'n', 'e', 'r', 'a', 't', 'o', 'r'],
+      uppercase: ['P', 'A', 'S', 'S', 'W', 'O', 'R', 'D', ' ', 'G', 'E', 'N', 'E', 'R', 'A', 'T', 'O', 'R'],
+    };
+
+    assert.deepEqual(toCharArray(strings[key]), expectedResults[key]);
   });
 }
